01f04a84421b8cac2f8b6027257cf3a2bd0e592b
Max <max.kruse93@googlemail.com>
2014-05-27 11:24:03 +0200

    27.5. 11:23

---
+++ PSMG_MadnessStudio/Assets/Scripts/GazeTestRotate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using iViewX;
+
+public class GazeTestRotate : MonoBehaviourWithGazeComponent {
+
+    public float rotationsPerMinute = 100.0f;
+
+    public override void OnGazeEnter(RaycastHit hit)
+    {
+        
+    }
+
+    public override void OnGazeStay(RaycastHit hit)
+    {
+        
+    }
+
+    public override void OnGazeExit()
+    {
+        
+    }
+}
