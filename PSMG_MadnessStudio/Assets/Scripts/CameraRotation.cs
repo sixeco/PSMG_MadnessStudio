d188edf93e00baaaebbe3dc900b2f41353d368ef
Max <max.kruse93@googlemail.com>
2014-05-28 13:47:30 +0200

    28.5. 13:47

--- PSMG_MadnessStudio/Assets/Scripts/CameraRotation.cs
+++ PSMG_MadnessStudio/Assets/Scripts/CameraRotation.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class CameraRotation : MonoBehaviour {
 
+    public Texture cursor;
+
     public float mouseSensitivity = 3.0f;
     public float upDownRange = 70.0f;
     public float leftRightRange = 70.0f;
     float verticalRotation = 0.0f;
     float horizontalRotation = 0.0f;
-    
+
+    void Start()
+    {
+        Screen.lockCursor = true;
+    }
+
 	void Update () {
 
         horizontalRotation += Input.GetAxis("Mouse X") * mouseSensitivity;
@@ -16,6 +23,7 @@ public class CameraRotation : MonoBehaviour {
 
         verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
         verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
-        gameObject.transform.localRotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0);
+        
+        //gameObject.transform.localRotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0);
 	}
 }
--- PSMG_MadnessStudio/Assets/Scripts/Shooter.cs
+++ PSMG_MadnessStudio/Assets/Scripts/Shooter.cs
@@ -29,7 +29,6 @@ public class Shooter : MonoBehaviour {
 
     void Start()
     {
-        Screen.lockCursor = true;
         shotMode = false;
     }
 
