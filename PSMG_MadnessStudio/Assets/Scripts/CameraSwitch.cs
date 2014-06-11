c658f3d8eb4bb8a0a61ffc46c6d5a0d05dc99641
Max <max.kruse93@googlemail.com>
2014-06-11 08:46:06 +0200

    Erster Kamera Switch

---
+++ PSMG_MadnessStudio/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitch : MonoBehaviour {
+
+    public Camera camera01;
+    public Camera camera02;
+
+	void Start () {
+        camera01.camera.enabled = true;
+        camera02.camera.enabled = false;
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            camera01.camera.enabled = true;
+            camera02.camera.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            camera01.camera.enabled = false;
+            camera02.camera.enabled = true;
+        }
+	}
+}
