13f3b9d029e422d3b488c1cb3aa62cbd73fc7b86
Max <max.kruse93@googlemail.com>
2014-06-18 10:54:35 +0200

    Level_0 part_01

---
+++ PSMG_MadnessStudio/Assets/Scripts/Levels/CoolDownValues.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoolDownValues : MonoBehaviour {
+
+    public float Gatling;
+    public float RocketMain;
+    public float LaserMain;
+}
