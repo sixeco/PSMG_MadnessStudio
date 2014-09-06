9859941e2e23b4a364ecbd7081a834131d01303b
Max <max.kruse93@googlemail.com>
2014-09-06 17:59:30 +0200

    2/4 Waffen vollständig implementiert

--- PSMG_MadnessStudio/Assets/Scripts/Levels/Turret/Weapons/Rabbit_TwinCannon.cs
+++ PSMG_MadnessStudio/Assets/Scripts/Levels/Turret/Weapons/Rabbit_TwinCannon.cs
@@ -5,26 +5,66 @@ public class Rabbit_TwinCannon : MonoBehaviour {
 
     public Transform UpperShotPos;
     public Transform LowerShotPos;
+    public Camera Camera;
 
     private float CoolDown;
     private float coolDownRemain;
+    private float shotForce;
+
     private Rigidbody projectile;
     private GameObject flash;
 
+    private bool turn;
+    private float RayCheckRange = 300.0f;
+
     void Awake()
     {
-        projectile = GameObject.Find("Data").GetComponent<ModelData>().CannonProjectile;
-        flash = GameObject.Find("Data").GetComponent<ModelData>().CannonMuzzleFlash;
+        GameObject data = GameObject.Find("Data");
+        projectile = data.GetComponent<ModelData>().CannonProjectile;
+        flash = data.GetComponent<ModelData>().CannonMuzzleFlash;
+        CoolDown = data.GetComponent<CoolDownValues>().Cannon;
+        shotForce = data.GetComponent<DamageData>().RabbitCannonForce;
+    }
+
+    void Start()
+    {
+        turn = true;
+        coolDownRemain = 0;
     }
 
     void Update()
     {
-
+        coolDownRemain -= Time.deltaTime;
     }
 
     public void Shoot(Vector2 direction)
     {
-        Debug.Log("Rabbit shot" + direction);
-        Instantiate(flash, UpperShotPos.position, Quaternion.identity);
+        if (coolDownRemain <= 0)
+        {
+            Ray ray = Camera.ScreenPointToRay(new Vector3(direction.x, Screen.height - direction.y, 0));
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo, RayCheckRange))
+            {
+                if (turn)
+                {
+                    Instantiate(flash, UpperShotPos.position, Quaternion.identity);
+                    Rigidbody upperShot = Instantiate(projectile, UpperShotPos.position, UpperShotPos.rotation) as Rigidbody;
+                    Vector3 UpperShotVector = hitInfo.point - upperShot.transform.position;
+                    upperShot.AddForce(UpperShotVector * shotForce, ForceMode.Acceleration);
+                    turn = !turn;
+                }
+                else
+                {
+                    Instantiate(flash, LowerShotPos.position, Quaternion.identity);
+                    Rigidbody lowerShot = Instantiate(projectile, LowerShotPos.position, LowerShotPos.rotation) as Rigidbody;
+                    Vector3 LowerShotVector = hitInfo.point - lowerShot.transform.position;
+                    lowerShot.AddForce(LowerShotVector * shotForce, ForceMode.Acceleration);
+                    turn = !turn;
+                }
+
+                coolDownRemain = CoolDown;
+            }
+        }
     }
 }
--- PSMG_MadnessStudio/Assets/Scripts/Levels/Turret/Weapons/Tiger_RocketLauncher.cs
+++ PSMG_MadnessStudio/Assets/Scripts/Levels/Turret/Weapons/Tiger_RocketLauncher.cs
@@ -3,17 +3,20 @@ using System.Collections;
 
 public class Tiger_RocketLauncher : MonoBehaviour {
 
-    private float CoolDownRocket;
+    private float CoolDown;
     private float CoolDownRemain;
-
     private GameObject RocketObject;
+    private GameObject flash;
+
     public Transform RocketLauncherShotPos;
+    public Camera camera;
 
     void Start()
     {
         CoolDownRemain = 0;
-        CoolDownRocket = GameObject.Find("Data").GetComponent<CoolDownValues>().RocketMain;
+        CoolDown = GameObject.Find("Data").GetComponent<CoolDownValues>().RocketMain;
         RocketObject = GameObject.Find("Data").GetComponent<ModelData>().RocketModel;
+        flash = GameObject.Find("Data").GetComponent<ModelData>().CannonMuzzleFlash;
     }
 
     void Update()
@@ -23,6 +26,17 @@ public class Tiger_RocketLauncher : MonoBehaviour {
 
     public void Shoot(Vector2 direction)
     {
-        Debug.Log("Tiger shot" + direction);
+        if (CoolDownRemain <= 0)
+        {
+            CoolDownRemain = CoolDown;
+            Ray ray = camera.ScreenPointToRay(new Vector3(direction.x, Screen.height - direction.y, 0));
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo, 300f))
+            {
+                Quaternion rotation = Quaternion.LookRotation((hitInfo.point - (RocketLauncherShotPos.transform.position + RocketLauncherShotPos.transform.forward)).normalized);
+                Instantiate(flash, RocketLauncherShotPos.position + RocketLauncherShotPos.up, Quaternion.identity);
+                Instantiate(RocketObject, RocketLauncherShotPos.transform.position, rotation);
+            }
+        }
     }
 }
